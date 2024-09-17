1ccf08acd15fb53e168ddaed5fd0e9daa49a30d2
Your Name <you@example.com>
2024-09-17 08:55:39 +0700

    Organization project

--- src/App.js
+++ src/App.js
@@ -1,92 +1,17 @@
+import React from 'react';
 import './App.scss';
+
+// custom components
+import AppBar from 'components/AppBar/AppBar';
+import BoarBar from 'components/BoarBar/BoarBar';
+import BoarContent from 'components/BoardContent/BoardContent';
 
 function App() {
   return (
     <div className="master">
-      <nav className='navbar app'>App Bar</nav>
-      <nav className='navbar board'>Board Bar</nav>
-      <div className='board-columns'>
-      <div className='column'>
-       <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src="https://via.placeholder.com/150" 
-            alt="Placeholder Image" /><br></br>
-            Title: NguyenTH
-          </li>
-          <li>Never give up</li>
-          <li>Never give up</li>
-          <li>Hope</li>
-        </ul>
-        <footer>Add another card</footer>
-       </div>
-       <div className='column'>
-       <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src="https://via.placeholder.com/150" alt="Placeholder Image" />
-            <br></br>Title: NguyenTH
-          </li>
-          <li>Never give up</li>
-          <li>Never give up</li>
-          <li>Never give up</li>
-        </ul>
-        <footer>Add another card</footer>
-       </div>
-       <div className='column'>
-       <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src="https://via.placeholder.com/150" alt="Placeholder Image" />
-            <br></br>Title: NguyenTH
-          </li>
-          <li>Never give up</li>
-          <li>Never give up</li>
-          <li>Never give up</li>
-        </ul>
-        <footer>Add another card</footer>
-       </div>
-       <div className='column'>
-       <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src="https://via.placeholder.com/150" alt="Placeholder Image" />
-            <br></br>Title: NguyenTH
-          </li>
-          <li>Never give up</li>
-          <li>Never give up</li>
-          <li>Never give up</li>
-        </ul>
-        <footer>Add another card</footer>
-       </div>
-       <div className='column'>
-       <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src="https://via.placeholder.com/150" alt="Placeholder Image" />
-            <br></br>Title: NguyenTH
-          </li>
-          <li>Never give up</li>
-          <li>Ican </li>
-          <li>Really</li>
-        </ul>
-        <footer>Add another card</footer>
-       </div>
-       <div className='column'>
-       <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src="https://via.placeholder.com/150" alt="Placeholder Image" />
-            <br></br>Title: NguyenTH
-          </li>
-          <li>Never give up</li>
-          <li>Never give up</li>
-          <li>Never give up</li>
-        </ul>
-        <footer>Add another card</footer>
-       </div>
-       
-      </div>
+      <AppBar/>
+      <BoarBar/>
+      <BoarContent/>
     </div>
   );
 }
---
+++ src/components/Task/Task.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Task.scss';
+
+function Task(){
+    return (
+        <li className="task-item">
+          <img src="https://via.placeholder.com/150" 
+          alt="Placeholder Image" /><br></br>
+          Title: NguyenTH
+        </li>
+    )
+}
+
+export default Task
