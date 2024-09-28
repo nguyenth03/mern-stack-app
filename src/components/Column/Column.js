a7a0547f33f262e2881a94ce74df947be082d4c9
Your Name <you@example.com>
2024-09-28 12:13:22 +0700

    drag drop column, card

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,22 +1,74 @@
 import React from "react";
 
+import { Container, Draggable } from 'react-smooth-dnd';
+import PropTypes from "prop-types"; // Import PropTypes
 import './Column.scss';
 import Card from "components/Card/Card";
 import { mapOrder } from "utilities/sorts";
 
+function Column(props) {
+    const { column } = props;
+    const cards = mapOrder(column.cards, column.cardOrder, 'id');
 
-function Column(props){
-    const { column } = props
-    const cards = mapOrder(column.cards, column.cardOrder,'id')
+
+    const onCardnDrop = (dropResult) => {
+        console.log(dropResult)
+    }
     return (
-        <div className='column'>
-         <header>{column.title}</header>
-         <ul className="card-list">
-            {cards.map((card, index) => <Card key={index} card={card} />)}
-        </ul>
-          <footer>Add another card</footer>
-         </div>
-    )
+        <div className='column-drag-handle'>
+            <header>{column.title}</header>
+            <div className="card-list">
+            <Container
+                    // onDragStart={e => console.log("drag started", e)}
+                    // onDragEnd={e => console.log("drag end", e)}
+                    // onDragEnter={() => {
+                    //   console.log("drag enter:", column.id);
+                    // }}
+                    // onDragLeave={() => {
+                    //   console.log("drag leave:", column.id);
+                    // }}
+                    groupName="col"
+                    onDrop={onCardnDrop}
+                    getChildPayload={index => cards[index]}
+                    dragClass="card-ghost"
+                    dropClass="card-ghost-drop"
+                    onDropReady={p => console.log('Drop ready: ', p)}
+                    dropPlaceholder={{                      
+                      animationDuration: 150,
+                      showOnTop: true,
+                      className: 'card-drop-preview' 
+                    }}
+                    dropPlaceholderAnimationDuration={200}
+                  >
+                {cards.map((card, index) => (
+                    <Draggable key={index}>
+                         <Card card={card} />
+                    </Draggable>
+                ))} {/* Use card.id for the key */}
+                </Container>
+            </div>
+            <footer>Add another card</footer>
+        </div>
+    );
 }
 
-export default Column
+// Define prop types for Column component
+Column.propTypes = {
+    column: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        boardId: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        cardOrder: PropTypes.arrayOf(PropTypes.string).isRequired,
+        cards: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.string.isRequired,
+                boardId: PropTypes.string.isRequired,
+                columnId: PropTypes.string.isRequired,
+                title: PropTypes.string.isRequired,
+                cover: PropTypes.string // or PropTypes.any if cover can be of different types
+            })
+        ).isRequired,
+    }).isRequired,
+};
+
+export default Column;
